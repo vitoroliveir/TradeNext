6abbf70c17d4174df8ff9365854798aed8ae3f0a
vitoroliveir <vitoroliveira732@gmail.com>
2022-12-25 20:54:26 -0300

    loading

--- src/components/Loading/styles.js
+++ src/components/Loading/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    margin-top: 35%;
+    width: 10%;
+    height: 10%;
+    margin-top: 40vh;
     display: flex;
     align-items: center;
     justify-content: center;
     position:absolute;
+    left:45vw;
 `;
